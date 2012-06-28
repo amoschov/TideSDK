4436fba306653c42f3b287182ba9f39020186ab7
Steven Verbeek <sverbeek@modernmedia.ca>
2012-06-28 02:13:35 -0300

    Fixed globals test, changed version number

--- tools/drillbit/Resources/tests/_globals/_globals.js
+++ tools/drillbit/Resources/tests/_globals/_globals.js
@@ -13,7 +13,7 @@ describe("ti global tests",
 	{
 		value_of(Titanium.version).should_be_string();
 		var arr = Titanium.version.split('.');
-		value_of(arr.length).should_be(3);
+		value_of(arr.length).should_be(4);
 		value_of(Number(arr[0])).should_be_number();
 		value_of(Number(arr[1])).should_be_number();
 		value_of(Number(arr[2])).should_be_number();
